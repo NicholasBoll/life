191a8c407fde992462e3478b315deb43c31d5cc2
Nicholas Boll <nicholas.boll@gmail.com>
2012-06-29 02:06:10 -0700

    Added ability to start and stop - makes it much more interesting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
-/*globals document, Cell, Grid, Board*/
+/*globals document, Cell, Grid, Board, setInterval*/
 
 var App = (function(){
   "use strict";
 
   var inputGrid = new Grid(50, 50),
       board = new Board(document.getElementById('main'), inputGrid),
-      generation = 1;
+      generation = 1,
+      interval;
 
   var parseInput = function () {
 
@@ -22,6 +23,17 @@ var App = (function(){
 
     inputGrid.nextGeneration();
     board.draw(inputGrid);
+  });
+
+  document.getElementById('start').addEventListener('click', function () {
+    interval = setInterval(function () {
+      inputGrid.nextGeneration();
+      board.draw(inputGrid);
+    }, 200);
+  });
+
+  document.getElementById('stop').addEventListener('click', function () {
+    clearInterval(interval);
   });
 
 
